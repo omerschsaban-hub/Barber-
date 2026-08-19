1598c1a33bca9dcb6dd5b3f29eeacd728325643a
omerschsaban-hub <omerschsaban@gmail.com>
2026-08-20 00:12:04 +0300

    Expose risk map in composed engineering app

--- engineering/app/composed.py
+++ engineering/app/composed.py
@@ -2,6 +2,7 @@ from .main import app
 from .advanced import router as advanced_router
 from .real_cv_sim2real import router as real_cv_sim2real_router
 from .cv_json import router as cv_json_router
+from .risk_map import router as risk_map_router
 from .validate_dimension import router as validate_dimension_router
 from .manufacturing import router as manufacturing_router
 
@@ -9,5 +10,6 @@ from .manufacturing import router as manufacturing_router
 app.include_router(advanced_router)
 app.include_router(real_cv_sim2real_router)
 app.include_router(cv_json_router)
+app.include_router(risk_map_router)
 app.include_router(validate_dimension_router)
 app.include_router(manufacturing_router)
