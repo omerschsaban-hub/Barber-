3bdd0eee910a02175bb49f422684756939afcadd
omerschsaban-hub <omerschsaban@gmail.com>
2026-08-20 14:05:52 +0300

    Wire internal product improvement loop into engineering API

--- engineering/app/composed.py
+++ engineering/app/composed.py
@@ -16,6 +16,7 @@ from .data_flywheel_worker import router as data_flywheel_worker_router
 from .moat_intelligence import router as moat_intelligence_router
 from .integration_gateway import router as integration_gateway_router
 from .product_intelligence import install_product_intelligence
+from .competitive_product_loop import router as competitive_product_loop_router
 
 app.include_router(advanced_router)
 app.include_router(real_cv_sim2real_router)
@@ -32,5 +33,6 @@ app.include_router(data_flywheel_router)
 app.include_router(data_flywheel_worker_router)
 app.include_router(moat_intelligence_router)
 app.include_router(integration_gateway_router)
+app.include_router(competitive_product_loop_router)
 install_product_intelligence(app)
 install_universal_quality(app)
